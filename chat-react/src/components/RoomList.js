994fafda80158d4ba09a6a549e431a20927bf355
Stephan Griesel <sgriesel@gmail.com>
2019-01-04 06:37:48 +0100

    update roomlist component

--- chat-react/src/components/RoomList.js
+++ chat-react/src/components/RoomList.js
@@ -5,7 +5,16 @@ class RoomList extends React.Component {
       console.log(this.props.rooms)
     return (
       <div className="rooms-list">
-        <div className="help-text">RoomList</div>
+        <ul>
+            <h3>Your Rooms:</h3>
+            {this.props.rooms.map(room => {
+                return (
+                    <li key={room.id} className="room">
+                        <a href="#"># {room.name}</a>
+                    </li>
+                )
+            })}
+        </ul>
       </div>
     )
   }
