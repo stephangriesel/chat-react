988f9b38c597b75f6bf44b50f9a910d5e6b46ba4
Stephan Griesel <sgriesel@gmail.com>
2019-01-05 08:40:21 +0100

    update newroomform component

--- chat-react/src/components/NewRoomForm.js
+++ chat-react/src/components/NewRoomForm.js
@@ -1,11 +1,30 @@
 import React from 'react'
 
 class NewRoomForm extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            roomName:''
+        }
+    }
+
+    handleChange(e) {
+        this.setState({
+            roomName: e.target.value
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+    }
+
   render() {
     return (
       <div className="new-room-form">
-        <form>
+        <form onSubmit={this.handleSubmit}>
             <input 
+            onChange={this.handleChange}
             type="text"
             placeholder="newRoomForm"
             required
