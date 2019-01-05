8403ac0bb5891e6a075bd3e62e44791971166836
Stephan Griesel <sgriesel@gmail.com>
2019-01-05 08:29:39 +0100

    add autoscroll

--- chat-react/src/components/MessageList.js
+++ chat-react/src/components/MessageList.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import Message from './Message';
+import React from "react";
+import Message from "./Message";
+import ReactDOM from "react-dom";
 
 class MessageList extends React.Component {
-    render() {
-        return (
-            <div className="message-list">
-                {this.props.messages.map((message,index) => {
-                    return (
+  componentWillUpdate() {
+    const node = ReactDOM.findDOMNode(this);
+    this.shouldScrollToBottom =
+      node.scrollTop + node.clientHeight + 100 >= node.scrollHeight;
+  }
 
-                        <Message key={index} username={message.senderId} text={message.text} />
-
-                    )
-                })}
-            </div>
-        )
+  componentDidUpdate() {
+    if (this.shouldScrollToBottom) {
+      const node = ReactDOM.findDOMNode(this);
+      node.scrollTop = node.scrollHeight;
     }
+  }
+
+  render() {
+    return (
+      <div className="message-list">
+        {this.props.messages.map((message, index) => {
+          return (
+            <Message
+              key={index}
+              username={message.senderId}
+              text={message.text}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default MessageList;
-
